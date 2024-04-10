8293e6a6311d1ac65ae87b28a9af06bd66d11c88
高橋舞 <tkjnpamw5611@gmail.com>
2024-04-11 04:19:22 +0900

    useLocation フックを使ってみる

--- sample-vite/src/Page1DetailA.jsx
+++ sample-vite/src/Page1DetailA.jsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 export const Page1DetailA = () => {
+    const { id } = useParams();
+    console.log(id); // パラメータ "id" の値をログに出力
+
     return (
         <div>
             <h1>Page1DetailAページです</h1>
+            <p>Parameter: {id}</p>
         </div>
     );
 };
