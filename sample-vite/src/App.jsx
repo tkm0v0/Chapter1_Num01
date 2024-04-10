0b643144c1d68632ad78a91c4394f6b41edd646c
高橋舞 <tkjnpamw5611@gmail.com>
2024-04-11 04:59:19 +0900

    section5-21 wip: useMatchフックを使用

--- sample-vite/src/App.jsx
+++ sample-vite/src/App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './Home';
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
 
 function App() {
   return (
@@ -19,11 +17,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} >
-          <Route path="detailA" element={<Page1DetailA />} />
-          <Route path="detailB" element={<Page1DetailB />} />
-
-        </Route>
+        <Route path="/page1/*" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
@@ -44,3 +38,5 @@ export default App;
 
 //React Router v6 以降でロケーションやパラメータにアクセスする方法
 //useLocation フックと useParams フックを使用
+
+//React Router v6 以降では、match オブジェクトの代わりに useMatch フックを使用
