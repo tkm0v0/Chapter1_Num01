fce2b33d4cfbf8b4e96b11564e9345bf2cb875f0
高橋舞 <tkjnpamw5611@gmail.com>
2024-04-11 02:46:22 +0900

    section5-19,20 done

--- sample-vite/src/App.jsx
+++ sample-vite/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// インポートするコンポーネント
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Link to="/">Home</Link><br />
+        <Link to="/page1">Page1</Link><br />
+        <Link to="/page2">Page2</Link><br />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
+//React Router version 6以降の書き方
+// Switchの代わりにRoutesを使用する: React Router version 6では、Switchコンポーネントは廃止され、Routesコンポーネントに置き換えられました。これにより、ルートの定義がより明確になり、ネストされたルーティングが簡単になります。
+
+// Routeの子要素にコンポーネントを直接指定するのではなく、elementプロパティを使用する: Version 6では、ルートに対するコンポーネントはelementプロパティを通して渡され、JSX形式で記述する必要があります。
+
+//、Linkコンポーネントにexactプロパティは使用されません。exactプロパティは、以前のバージョンのReact Router（特にversion 5以前）でRouteコンポーネントにおいて、パスの完全一致を指定するために使用されていました。しかし、React Router version 6ではRouteのマッチングシステムが改善され、Routesコンポーネント内のRouteが自動的に正確なマッチングを行うため、このプロパティは不要になりました。
+// そのため、Linkコンポーネントのexactプロパティは削除するのが適切です。
