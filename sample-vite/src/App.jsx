3138eac89e905fc7f8c72276942bb07b646942a0
高橋舞 <tkjnpamw5611@gmail.com>
2024-04-11 03:50:10 +0900

    ページ遷移は正常にできているが、`Page1Detail A``Page1Detail B`コンポーネントの内容が画面に表示(レンダリング)されない。

--- sample-vite/src/App.jsx
+++ sample-vite/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { Home } from './Home';
 import { Page1 } from './Page1';
 import { Page2 } from './Page2';
+import { Page1DetailA } from './Page1DetailA';
+import { Page1DetailB } from './Page1DetailB';
 
 function App() {
   return (
@@ -11,11 +13,21 @@ function App() {
       <div>
         <Link to="/">Home</Link><br />
         <Link to="/page1">Page1</Link><br />
+        <Link to="/page1/detailA">Page1 Detail A</Link><br />
+        <Link to="/page1/detailB">Page1 Detail B</Link><br />
         <Link to="/page2">Page2</Link><br />
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page1" element={<Page1 />} >
+             {/* パターン1 */}
+            {/* <Route path="/page1/detailA" element={<Page1DetailA />} />
+            <Route path="/page1/detailB" element={<Page1DetailB />} /> */}
+             {/* パターン2 */}
+          <Route path="detailA" element={<Page1DetailA />} />
+          <Route path="detailB" element={<Page1DetailB />} />
+
+        </Route>
         <Route path="/page2" element={<Page2 />} />
       </Routes>
     </BrowserRouter>
--- sample-vite/src/Page1.jsx
+++ sample-vite/src/Page1.jsx
@@ -1,7 +1,10 @@
+import {Link} from "react-router-dom";
 export const Page1 = () => {
     return (
         <div>
-            <h1>Page1</h1>
+            <h1>Page1ページです</h1>
+            <Link to="/page1/detailA">DetailA</Link><br />
+            <Link to="/page1/detailB">DetailB</Link><br />
         </div>
     );
 };
