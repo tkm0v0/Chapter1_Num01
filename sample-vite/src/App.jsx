d62fdfd9a841727b765a3d81504f0b8cde8a6b8f
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-09 03:37:45 +0900

    section5-22_`src/router/Router.jsx`ファイルへの分割

--- sample-vite/src/App.jsx
+++ sample-vite/src/App.jsx
@@ -1,14 +1,10 @@
-import { BrowserRouter ,Link ,Switch ,Route} from 'react-router-dom';
+import { BrowserRouter ,Link } from 'react-router-dom';
 //BrowserRouterタグ配下で ルーティングが有効になる
 //Link = <a>タグに相当するもの
 //Switch = コンポーネントを出し分ける
 //Route = ルーティングの設定  //Switchの中で、Routeにマッチしたものを表示する
 
-import { Home } from './Home';
-import { Page1 } from './Page1';
-import { Page2 } from './Page2';
-import { Page1DetailA } from './Page1DetailA';
-import { Page1DetailB } from './Page1DetailB';
+import { Router } from './router/Router';
 
 export default function App() {
     return (
@@ -20,28 +16,7 @@ export default function App() {
               <br />
               <Link to="/Page2">Page2</Link>
           </div>
-          <Switch>
-              <Route exact path="/"><Home /></Route>
-              <Route path="/Page1" 
-                //Page1 を関数に統一する → props内 matchから、path + url情報を取得する
-                render={({match : { url }})=> (
-                    //matchからulrの情報を受け取る
-                    <Switch>
-                      {console.log(url)}
-                        <Route exact path={url}>
-                          <Page1 /> 
-                        </Route>
-                        <Route path={`${url}/detailA`}>
-                          <Page1DetailA /> 
-                        </Route>
-                        <Route path={`${url}/detailB`}>
-                            <Page1DetailB /> 
-                        </Route>
-                    </Switch>
-                  ) }
-                  />
-              <Route path="/Page2"><Page2 /></Route>
-          </Switch>
+          <Router  />
       </BrowserRouter>
     );
 }
---
+++ sample-vite/src/router/Router.jsx
@@ -0,0 +1,33 @@
+import {Switch ,Route} from 'react-router-dom';
+import { Home } from '../Home';
+import { Page1 } from '../Page1';
+import { Page2 } from '../Page2';
+import { Page1DetailA } from '../Page1DetailA';
+import { Page1DetailB } from '../Page1DetailB';
+
+export const Router = () => {
+    return (
+        <Switch>
+        <Route exact path="/"><Home /></Route>
+        <Route path="/Page1" 
+          //Page1 を関数に統一する → props内 matchから、path + url情報を取得する
+          render={({match : { url }})=> (
+              //matchからulrの情報を受け取る
+              <Switch>
+                {console.log(url)}
+                  <Route exact path={url}>
+                    <Page1 /> 
+                  </Route>
+                  <Route path={`${url}/detailA`}>
+                    <Page1DetailA /> 
+                  </Route>
+                  <Route path={`${url}/detailB`}>
+                      <Page1DetailB /> 
+                  </Route>
+              </Switch>
+            ) }
+            />
+        <Route path="/Page2"><Page2 /></Route>
+    </Switch>
+    );
+}
