64a23d769bd24f9cdc3434bc7ff538f8e0809349
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-09 04:42:06 +0900

    section5-22_`src/router/Router.jsx`→`src/router/Page1Routes.jsx`ファイルへの分割、配列でループさせる

--- sample-vite/src/router/Router.jsx
+++ sample-vite/src/router/Router.jsx
@@ -1,29 +1,36 @@
 import { Switch, Route } from 'react-router-dom';
 import { Home } from '../Home';
-import { Page1 } from '../Page1';
+
 import { Page2 } from '../Page2';
-import { Page1DetailA } from '../Page1DetailA';
-import { Page1DetailB } from '../Page1DetailB';
+
+import { page1Routes } from './Page1Routes';
+
+//exactとは?
+//<Route />にexactを記述すると、pathに指定したパス文字列と、location.pathName（windowオブジェクトのlocation.pathName）が完全一致した場合のみコンポーネントを返すようになります。
+//[React Routerのexactとは何か](https://qiita.com/gombeedoe/items/6f4ea0e37775bac01c0d)
 
 export const Router = () => {
     return (
         <Switch>
             <Route exact path="/"><Home /></Route>
-            <Route path="/Page1"
+            <Route 
+                path="/Page1"
                 //Page1 を関数に統一する → props内 matchから、path + url情報を取得する
                 render={({ match: { url } }) => (
                     //matchからulrの情報を受け取る
                     <Switch>
-                        {console.log(url)}
-                        <Route exact path={url}>
-                            <Page1 />
-                        </Route>
-                        <Route path={`${url}/detailA`}>
-                            <Page1DetailA />
-                        </Route>
-                        <Route path={`${url}/detailB`}>
-                            <Page1DetailB />
-                        </Route>
+                        {
+                            //Page1のルーティング情報を取得
+                            page1Routes.map((route) => (
+                                <Route
+                                    key={route.path}
+                                    exact={route.exact}
+                                    path={`${url}${route.path}`}
+                                >
+                                    {route.children}
+                                </Route>
+                            ))
+                        }
                     </Switch>
                 )}
             />
