5a6e667900a0fce2103562d9e0530759ae301fb5
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-09 03:12:00 +0900

    section5-21_[Page1DetailBページ]のレイアウトを追加

--- sample-vite/src/Page1DetailB.jsx
+++ sample-vite/src/Page1DetailB.jsx
@@ -1,8 +1,11 @@
-
+import { Link } from "react-router-dom";
 export const Page1DetailB = () => {
     return (
         <div>
             <h1>Page1DetailBページです</h1>
+            <Link to="/page1/detailA">DetailA</Link>
+            <br />
+            <Link to="/page1/detailB">DetailB</Link>
         </div>
     );
 }
