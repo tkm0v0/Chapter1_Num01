dbc6c1b4fba35f0a8d9ae1df578922c3b34daae2
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 12:26:35 +0900

    section5_24.ページ遷移のリンクを追加

--- sample-vite/src/Page2.jsx
+++ sample-vite/src/Page2.jsx
@@ -4,6 +4,8 @@ export const Page2 = () => {
         <div>
             <h1>Page2ページです</h1>
             <Link to="/Page2/999">URL Parameter</Link>
+            <br/>
+            <Link to="/Page2/999?name=hogehoge">Query Parameter</Link>
         </div>
     );
 }
