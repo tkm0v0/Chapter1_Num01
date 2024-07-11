d304194bcba44935afca3ddcb50911ced4477d40
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 12:26:22 +0900

    section5_23.ページ遷移のリンクを追加

--- sample-vite/src/Page2.jsx
+++ sample-vite/src/Page2.jsx
@@ -1,8 +1,9 @@
-
+import { Link } from "react-router-dom";
 export const Page2 = () => {
     return (
         <div>
             <h1>Page2ページです</h1>
+            <Link to="/Page2/999">URL Parameter</Link>
         </div>
     );
 }
