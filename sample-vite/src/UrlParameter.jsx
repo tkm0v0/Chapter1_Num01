d4b47e0db81cbba8c2f0394d3b83db6e0e4e578a
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 12:44:26 +0900

    section5_24.クエリパラメーターをフロントに表示

--- sample-vite/src/UrlParameter.jsx
+++ sample-vite/src/UrlParameter.jsx
@@ -1,12 +1,14 @@
-import { Link, useParams  } from "react-router-dom";
+import { useParams, useLocation  } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParams();
+    const location = useLocation();
+    console.log(location);
     return (
         <div>
             <h1>UrlParameterページです</h1>
-            <Link to="/Page2/999">URL Parameter</Link>
             <p>パラメーターは{id}です</p>
+            <p>クエリパラメーターは{ location.search ? `${location.search}です` : "ありません"}</p>
         </div>
     );
 }
