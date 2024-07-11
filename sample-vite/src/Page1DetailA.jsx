955424d21f1609e8e9d2ead076f243e3b0544049
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 15:38:23 +0900

    section5_26.`useHistory`を使う

--- sample-vite/src/Page1DetailA.jsx
+++ sample-vite/src/Page1DetailA.jsx
@@ -1,10 +1,16 @@
-import { useLocation } from "react-router-dom";
+import { useLocation,useHistory } from "react-router-dom";
 export const Page1DetailA = () => {
     const location = useLocation();
     console.log(location);
+
+    const history = useHistory();
+    const onClickBack = () => history.goBack();
+
     return (
         <div>
             <h1>Page1DetailAページです</h1>
+            <br />
+            <button onClick={onClickBack}>戻る</button>
         </div>
     );
 }
