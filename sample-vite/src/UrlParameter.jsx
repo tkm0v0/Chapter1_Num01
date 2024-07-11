070061c50e473658d7c1b13167d746bb38e4bce4
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 13:37:26 +0900

    section5_24.クエリパラメーターをフロントに表示,テスト

--- sample-vite/src/UrlParameter.jsx
+++ sample-vite/src/UrlParameter.jsx
@@ -2,14 +2,15 @@ import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParams();
-    const { search } = useLocation();
-    const query = new URLSearchParams(search);
-    console.log(query);
+    const location  = useLocation();
+    const query = new URLSearchParams("uselocation():"+location.search);
+    console.log("query:"+query);
     return (
         <div>
             <h1>UrlParameterページです</h1>
             <p>パラメーターは{id}です</p>
-            <p>クエリパラメーターは{query.get("name")}です</p>
+            {/* <p>クエリパラメーターは{query.get("name")}です</p> */}
+            <p>クエリパラメーターは{query.get("name")?`${query.get("name")}です` : "存在しません"}</p>
         </div>
     );
 }
