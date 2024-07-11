9f12efb5446926ac5bf748d393a3f29c6924370f
高橋舞 <tkjnpamw5611@gmail.com>
2024-07-11 13:37:48 +0900

    もとに戻す

--- sample-vite/src/UrlParameter.jsx
+++ sample-vite/src/UrlParameter.jsx
@@ -2,15 +2,14 @@ import { useParams, useLocation } from "react-router-dom";
 
 export const UrlParameter = () => {
     const { id } = useParams();
-    const location  = useLocation();
-    const query = new URLSearchParams("uselocation():"+location.search);
-    console.log("query:"+query);
+    const { search } = useLocation();
+    const query = new URLSearchParams(search);
+    console.log(query);
     return (
         <div>
             <h1>UrlParameterページです</h1>
             <p>パラメーターは{id}です</p>
-            {/* <p>クエリパラメーターは{query.get("name")}です</p> */}
-            <p>クエリパラメーターは{query.get("name")?`${query.get("name")}です` : "存在しません"}</p>
+            <p>クエリパラメーターは{query.get("name")}です</p>
         </div>
     );
 }
